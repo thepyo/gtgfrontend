d1167508c4c05e4f8629bbfea3dba9b7f6c7fe8c
tuanpinggo <tuannh@pinggo.vn>
2024-09-19 11:33:24 +0700

    test tut trict

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,24 @@
 import dynamic from 'next/dynamic';
+import { useEffect, useState } from 'react';
 const EmptyLayout =  dynamic(() => import('@/layouts/empty'))
 const YoungBussinessHomePageSection = dynamic(() => import('@/components/pages/homepage/youngBussiness'))
 
 export default function Index() {
-  return(
+
+  const [loading,setLoading] = useState(true)
+
+  useEffect(()=>{
+
+    let timer = setTimeout(() => setLoading(false), 500)
+
+    return () => clearTimeout(timer)
+  },[])
+
+  if(!loading) return(
     <YoungBussinessHomePageSection />
   )
+
+  return <>loading</>
 }
 
 Index.Layout = EmptyLayout
